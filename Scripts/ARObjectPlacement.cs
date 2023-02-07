4fcf95a451dd211226c6bd941f0fbb5debcac981
Sudhanshu Ranjan <109692241+rai-sudhanshu@users.noreply.github.com>
2023-02-07 20:51:54 +0530

    Add files via upload

---
+++ Scripts/ARObjectPlacement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+public class ARObjectPlacement : MonoBehaviour
+{
+    public ARSessionOrigin ar_session_origin;
+    public List<ARRaycastHit> raycastHits = new List<ARRaycastHit>();
+    public GameObject cube;
+    GameObject instantiatedCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            bool collision = ar_session_origin.GetComponent<ARRaycastManager>().Raycast(Input.mousePosition, raycastHits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon);
+            if (collision)
+            {
+                if (instantiatedCube == null)
+                {
+                    instantiatedCube = Instantiate(cube);
+                    foreach (var plane in ar_session_origin.GetComponent<ARPlaneManager>().trackables)
+                    {
+                        plane.gameObject.SetActive(false);
+                    }
+                    ar_session_origin.GetComponent<ARPlaneManager>().enabled = false;
+                }
+                instantiatedCube.transform.position = raycastHits[0].pose.position;
+            }
+        }
+    }
+}
